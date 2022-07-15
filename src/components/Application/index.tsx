ae3424034a6ae8ed5ca2e21750df44b276c8d739
Louis Young <me@louisyoung.co.uk>
2022-07-15 14:07:37 +0100

    Add Tailwind Prettier plugin

--- src/components/Application/index.tsx
+++ src/components/Application/index.tsx
@@ -1,7 +1,7 @@
 export const Application = () => {
   return (
     <main className="p-8">
-      <h1 className="text-gray-800 font-bold text-3xl">Application</h1>
+      <h1 className="text-3xl font-bold text-gray-800">Application</h1>
     </main>
   );
 };
