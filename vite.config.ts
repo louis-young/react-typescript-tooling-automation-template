ec4aeee93ccfb8c1ebf9f5601f30d225077fb97b
Louis Young <me@louisyoung.co.uk>
2022-09-30 10:33:13 +0100

    Add tooling terminal integration and error overlay

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
+import { checker } from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        // prettier-ignore
+        lintCommand: "eslint \"./**/*.{js,jsx,ts,tsx}\"",
+      },
+    }),
+  ],
   server: {
     host: true,
     open: true,
